0c73a1562a863310e5e406e3c6f76fa202e4ff23
Seball8798 <sebastianll0907@gmail.com>
2023-01-13 09:21:20 +0100

    V2

--- FIVEWORDSV2/Program.cs
+++ FIVEWORDSV2/Program.cs
@@ -40,44 +40,16 @@ namespace FiveWordFiveLetters
             return false;
         }
 
-
         static void Main(string[] args)
         {
-            string filePath = "5WORDS.txt";
-            var stopWatch = System.Diagnostics.Stopwatch.StartNew();
-            FindUniqueCombinations(filePath);
-            stopWatch.Stop();
-
-        }
-
-        private static int ToNumber(char c)
-        {
-            return c - 'a';
-        }
-        private static int WordToMask(string word)
-        {
-            const int EXPECTED_LENGTH = 5;
-            if (word.Length != EXPECTED_LENGTH) return 0;
-            int result = 0;
-
-            foreach (char c in word)
-            {
-                int bit = 1 << ToNumber(c);
-                if ((result & bit) != 0) return 0; // a letter occurred twice
-                result |= bit;
-            }
-            return result;
-        }
-
-
-
-
-
-        public static void FindUniqueCombinations(string filePath)
-        {
+            string filePath = "5words.txt";
+            Dictionary<int, string> _dictionary = new Dictionary<int, string>();
+            List<int> bitlist = new List<int>();
             string[] lines = File.ReadAllLines(filePath);
+
             var result = new List<string>();
-            const int EXPECTED_LENGTH = 5;
+
+
             var stopWatch = System.Diagnostics.Stopwatch.StartNew();
             stopWatch.Start();
             using (var reader = new StreamReader(filePath))
@@ -85,49 +57,72 @@ namespace FiveWordFiveLetters
                 string word = null;
                 while ((word = reader.ReadLine()) != null)
                 {
-                    if (word.Length != EXPECTED_LENGTH) continue;
+                    if (word.Length != 5) continue;
                     if (word.Distinct().Count() != word.Length) continue;
-                    if (result.Where(x => string.Concat(x, word).Distinct().Count() == 5).Count() > 0) continue;
-                    result.Add(word);
+                    // if (result.Where(x => string.Concat(x, word).Distinct().Count() == 5).Count() > 0) continue;
+                    //result.Add(word);
+
+                    int bitRepresentation = 0;
+                    foreach (char c in word) bitRepresentation += (int)1 << (c - 'a');
+
+                    if (_dictionary.ContainsKey(bitRepresentation)) continue;
+                    _dictionary.Add(bitRepresentation, word);
+                    bitlist.Add(bitRepresentation);
+
+
+
+
                 }
-                Console.WriteLine($"Words from input:  {result.Count}\n");
+                Console.WriteLine($"Words from input:  {bitlist.Count}\n");
             }
+
 
             int numberOfWords = 0;
 
-            for (int i = 0; i < result.Count; i++)
+            for (int i = 0; i < bitlist.Count; i++)
             {
 
-                for (int j = i + 1; j < result.Count; j++)
+                for (int j = i + 1; j < bitlist.Count; j++)
                 {
-                    if (string.Concat(result[i], result[j]).Distinct().Count() != 10) continue;
-                    for (int k = j + 1; k < result.Count; k++)
+                    if ((bitlist[i] | bitlist[j]) != 10) continue;
+                    for (int k = j + 1; k < bitlist.Count; k++)
                     {
-                        if (string.Concat(result[i], result[j], result[k]).Distinct().Count() != 15) continue;
+                        if ((bitlist[i] | bitlist[j] | bitlist[k]) != 15) continue;
 
-                        for (int l = k + 1; l < result.Count; l++)
+                        for (int l = k + 1; l < bitlist.Count; l++)
                         {
-                            if (string.Concat(result[i], result[j], result[k], result[l]).Distinct().Count() != 20) continue;
+                            if ((bitlist[i] | bitlist[j] | bitlist[k] | bitlist[l]) != 20) continue;
 
-                            for (int m = l + 1; m < result.Count; m++)
+                            for (int m = l + 1; m < bitlist.Count; m++)
                             {
 
-                                if (string.Concat(result[i], result[j], result[k], result[l], result[m]).Distinct().Count() != 25) continue;
+                                if ((bitlist[i] | bitlist[j] | bitlist[k] | bitlist[l] | bitlist[m]) != 25) continue;
 
-                                Console.WriteLine("\r{0} {1} {2} {3} {4}", result[i], result[j], result[k], result[l], result[m]);
+                                Console.WriteLine("\r{0} {1} {2} {3} {4}", bitlist[i], bitlist[j], bitlist[k], bitlist[l], bitlist[m]);
                                 numberOfWords++;
 
                             }
+
+                            //var binaryRepresentation = result.Select(word => string.Join("", Encoding.ASCII.GetBytes(word).Select(b => Convert.ToString(b, 2).PadLeft(8, '0'))));
+                            //Console.WriteLine(string.Join(" ", binaryRepresentation));
+                            //numberOfWords++;
+
+
                         }
                     }
                 }
             }
 
-            stopWatch.Stop();
 
             Console.WriteLine("\nNumber of combinations: " + numberOfWords);
-
+            stopWatch.Stop();
             Console.WriteLine("Task was completed in " + stopWatch.Elapsed.TotalMilliseconds + " milliseconds");
+
+
         }
+
     }
+
+
 }
+
